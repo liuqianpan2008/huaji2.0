65a00bd5b0ee346fbecaddc05b65f1c4ea278df3
枫叶秋林 <liuqianpan2018@outlook.com>
2021-07-26 18:25:38 +0800

    滑稽矿修复

--- src/main/java/com/fenglin/huaji/Item/ItemRegistry.java
+++ src/main/java/com/fenglin/huaji/Item/ItemRegistry.java
@@ -1,6 +1,9 @@
 package com.fenglin.huaji.Item;
 
+import com.fenglin.block.BlockRegistry;
+import com.fenglin.huaji.Group.ModGroup;
 import com.fenglin.huaji.Utils;
+import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
 import net.minecraftforge.fml.RegistryObject;
 import net.minecraftforge.registries.DeferredRegister;
@@ -9,5 +12,7 @@ import net.minecraftforge.registries.ForgeRegistries;
 public class ItemRegistry {
     public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, Utils.MOD_ID);
     public static final RegistryObject<Item> HUAJI = ITEMS.register("huaji", objhuaji::new);
+
+    public static final RegistryObject<Item> ORE_HUAJI = ITEMS.register("ore_huaji", () -> new BlockItem(BlockRegistry.ORK_HUAJI.get(), new Item.Properties().tab(ModGroup.itemGroup)));
 
 }
--- src/main/java/com/fenglin/huaji/huaji.java
+++ src/main/java/com/fenglin/huaji/huaji.java
@@ -1,5 +1,6 @@
 package com.fenglin.huaji;
 
+import com.fenglin.block.BlockRegistry;
 import com.fenglin.huaji.Item.ItemRegistry;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
@@ -8,5 +9,7 @@ import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 public class huaji {
     public huaji(){
         ItemRegistry.ITEMS.register(FMLJavaModLoadingContext.get().getModEventBus());
+
+        BlockRegistry.BLOCKS.register(FMLJavaModLoadingContext.get().getModEventBus());
     }
 }
